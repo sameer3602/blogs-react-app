428c1762a535c3784b3c5ef240d2112f38e38b55
Sameer Ali <sameerkhan.1234asd@gmail.com>
2024-03-25 22:31:26 +0530

    new commit

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,21 +5,23 @@ const Pagination = () => {
   const {page,handlepage,totalPages}=useContext(Appcontext);
   return (
     <div className='PaginationDiv'>
-        <div className='prevnextDiv'>
-          { page >1 &&
-            (<button onClick={()=>handlepage(page-1)} className='prevbtn button-30'> 
-              Previous
-            </button>)
-          }
-          { page<totalPages &&
-            (<button onClick={()=>handlepage(page+1)} className='nextbtn button-30' >
-              Next
-            </button>)
-          }
-          <p className='PageCount'>
-            Page {page} of {totalPages}
-          </p>
+        <div className='buttons'>
+            <div className='prevnextDiv'>
+              { page >1 &&
+                (<button onClick={()=>handlepage(page-1)} className='prevbtn button-30'> 
+                  Previous
+                </button>)
+              }
+              { page<totalPages &&
+                (<button onClick={()=>handlepage(page+1)} className='nextbtn button-30' >
+                  Next
+                </button>)
+              }
+            </div>
         </div>
+      <p className='PageCount'>
+          Page {page} of {totalPages}
+      </p>
     </div>
   )
 }
